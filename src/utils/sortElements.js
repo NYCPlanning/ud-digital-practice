16e6c5fe262d675f807be1551806b45e56903d8c
Carsten Rodin <carsten.rodin@gmail.com>
2020-08-26 18:28:57 -0400

    add support for non-image linked files

--- src/utils/sortElements.js
+++ src/utils/sortElements.js
@@ -4,10 +4,14 @@ export default (elements) => {
   const textTypes = ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ul', 'ol', 'li']
   let images = []
   let text = []
+  console.log(elements)
   
   React.Children.toArray(elements).forEach((child, i) => {
-    if (child.props.className === 'gatsby-resp-image-wrapper') images.push(child);
-    else if (textTypes.includes(child.props.mdxType)) text.push(child);
+    if (child.props.className === 'gatsby-resp-image-wrapper' || child.props.mdxType === 'img') {
+      images.push(child)
+    } else if (textTypes.includes(child.props.mdxType)) {
+      text.push(child);
+    }
   })
 
   return {text: text, images: images}
