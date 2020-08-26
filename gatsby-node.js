85748c764b581f57816dfcaa630269fde09ebd20
Carsten Rodin <carsten.rodin@gmail.com>
2020-08-26 18:27:41 -0400

    removes slug field requirement for processing  mdx slides

--- gatsby-node.js
+++ gatsby-node.js
@@ -72,7 +72,6 @@ exports.createPages = async ({ graphql, actions, reporter, pathPrefix }) => {
               title
               date
               desc
-              slug
             }
           }
         }
