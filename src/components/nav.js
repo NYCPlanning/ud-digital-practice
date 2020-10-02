52e273a20389dee8b201aa60c91dd5ca5c6ce8a5
Carsten Rodin <carsten.rodin@gmail.com>
2020-10-02 17:54:08 -0400

    downsize nav headings to h5, set up activeclassname on links

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,23 +2,23 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 const Nav = () => (
-  <div id='nav-column' className='flex flex-col'>
-    <h2>UDTools for Rhino</h2>
-    <Link to='/plugin/about'>About</Link>
-    <Link to='/plugin/install'>Install</Link>
-    <Link to='/plugin/guide'>Guide</Link>
+  <div id='nav-column' className='flex flex-col items-start'>
+    <h5>UDTools for Rhino</h5>
+    <Link activeClassName={'a-active'} to='/plugin/about'>About</Link>
+    <Link activeClassName={'a-active'} to='/plugin/install'>Install</Link>
+    <Link activeClassName={'a-active'} to='/plugin/guide'>Guide</Link>
 
-    <h2>NYC Digital Twin</h2>
-    <Link to='/digital-twin/about'>About</Link>
+    <h5>NYC Digital Twin</h5>
+    <Link activeClassName={'a-active'} to='/digital-twin/about'>About</Link>
 
-    <h2>Learning Resources</h2>
-    <Link to='/learning/rhino-basics'>Rhino Basics</Link>
+    <h5>Learning Resources</h5>
+    <Link activeClassName={'a-active'} to='/learning/rhino-basics'>Rhino Basics</Link>
 
-    <h2>Working With Us</h2>
-    <Link to='/collaborate/rwcds'>RWCDS Model Requirements</Link>
-    <Link to='/collaborate/roles'>Project Roles</Link>
-    <Link to='/collaborate/issues'>Submit an Issue</Link>
-    {/* <Link to='/digital-twin/about'>Skill Share</Link> */}
+    <h5>Working With Us</h5>
+    <Link activeClassName={'a-active'} to='/collaborate/rwcds'>RWCDS Model Requirements</Link>
+    <Link activeClassName={'a-active'} to='/collaborate/roles'>Project Roles</Link>
+    <Link activeClassName={'a-active'} to='/collaborate/issues'>Submit an Issue</Link>
+    {/* <Link activeClassName={'a-active'} to='/digital-twin/about'>Skill Share</Link> */}
   </div>
 )
 
