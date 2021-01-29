9cf0cf9f7ee59bcd687ae871accb62cc0b9ac9f5
Carsten Rodin <crodin@planning.nyc.gov>
2021-01-29 15:43:34 -0500

    1/29 deck update

--- src/components/iframe.js
+++ src/components/iframe.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
-export default ({ src }) => {
-  return (
+export default ({ src, mode }) => {
+
+  if ( mode === 'full' ) return (
+    <div className='fixed top-0 left-0 w-screen h-screen'>
+      <iframe src={src}
+        className='w-full h-full'
+        frameborder='0' 
+        allowfullscreen='' 
+        aria-hidden='false' 
+        tabindex='0'>
+      </iframe>
+    </div>
+  )
+  else return (
     <div className='w-full h-full p-4'>
       <iframe src={src}
         className='w-full h-full'
--- src/components/slides/imageRight.js
+++ src/components/slides/imageRight.js
@@ -5,7 +5,7 @@ export default ({ children }) => {
   const {text, images} = sortElements(children);
 
   return (
-    <div className='grid grid-cols-2 gap-2 h-full'>
+    <div className='grid grid-cols-2 gap-8 h-full'>
       <div className='flex flex-grow justify-center flex-col'>{text}</div>
       <div className='h-full flex items-center justify-center col-start-2'>{images}</div>
     </div>
