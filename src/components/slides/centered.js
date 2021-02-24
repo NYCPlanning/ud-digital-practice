5b9d66d15f7d3bb247fdde5f39f281e526f02cdc
Carsten Rodin <crodin@planning.nyc.gov>
2021-02-24 18:15:18 -0500

    removes unused slide deck components

--- src/components/slides/centered.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default ({ children }) => {
-  return (
-    <div className='w-full h-full flex items-center justify-center'>
-      {children}
-    </div>
-  )
-}
--- src/components/zola.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default () => (
-  <iframe
-    className='w-full h-full'
-    src='https://zola.planning.nyc.gov/' 
-    title='NYC Planning ZoLa'
-  />
-)
