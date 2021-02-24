1356e4675db8ead3a609c169e6d4fdb63cc4cd9e
Carsten Rodin <crodin@planning.nyc.gov>
2021-02-24 18:28:23 -0500

    installs netlify cms and accompanying gatsby plugin, creates config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-postcss`,
     `gatsby-plugin-styled-jsx`,
+    `gatsby-plugin-netlify-cms`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
