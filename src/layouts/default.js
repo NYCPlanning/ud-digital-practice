fc384f2a23ab841173e1e1e17b90fc4514b85575
Carsten Rodin <crodin@planning.nyc.gov>
2021-02-24 16:11:28 -0500

    removes header

--- src/layouts/default.js
+++ src/layouts/default.js
@@ -1,15 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Nav from '../components/nav'
-import Header from '../components/header'
 import SiteTitle from '../components/site_title'
 
 const Layout = ({ children, siteMetadata }) => {
   return(
     <div id='root' className="grid grid-cols-1 md:grid-cols-3 max-w-3xl p-8">
-     <div className='col-start-1 md:col-span-3 mb-8'>
-      <Header siteMetadata={siteMetadata} />
-     </div>
      <div className='mb-8'>
       <Nav siteMetadata={siteMetadata} />
      </div>
