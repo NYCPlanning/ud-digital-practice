2fe7656ecbbb093c80a381d307885a59f6eaf304
Carsten Rodin <crodin@planning.nyc.gov>
2021-03-12 14:53:08 -0500

    fixes site title display on safari

--- src/components/site_title.js
+++ src/components/site_title.js
@@ -3,8 +3,16 @@ import React from 'react'
 const SiteTitle = () => {
   return (
     <div className='gradient-bg fixed bottom-0 left-0 w-full p-4 pt-8 flex space-x-4'>
-        <img width={'72em'} src='https://raw.githubusercontent.com/NYCPlanning/dcp-logo/master/dcp_logo.svg' alt='DCP'/>
-        <h1>Urban Design Digital Practice</h1>
+        <div>
+          <img
+            width='72em'
+            src='https://raw.githubusercontent.com/NYCPlanning/dcp-logo/master/dcp_logo.svg' 
+            alt='DCP'
+          />
+        </div>
+        <div>
+          <h1>Urban Design Digital Practice</h1>
+        </div>
     </div>
   )
 }
