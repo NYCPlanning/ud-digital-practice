e1ce07e0fcd6fb45044e8f3b31301339ccb00444
Carsten Rodin <crodin@planning.nyc.gov>
2021-03-16 10:30:30 -0400

    adds 3d scene background to landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,11 @@ import Layout from '../layouts/default'
 
 const Index = () => {
   return (
-    <Layout>
-      Welcome!
+    <Layout mask={false} >
+      <iframe 
+        src='https://ud-interactive.netlify.app/doc-site-landing/'
+        className='fixed top-0 left-0 w-full h-full z-0 pointer-events-none'
+      />
     </Layout>
   )
 }
